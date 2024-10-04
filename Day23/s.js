78d6ca348e3279c058a4680032b28ee668bc2093
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-05 00:33:15 +0530

    learn object

---
+++ Day23/s.js
@@ -0,0 +1,19 @@
+// str = 'shivani' --> 'inavihs'
+let str="Shivani";
+let reverse="";
+console.log(str.charAt(0));
+console.log(str.length);
+
+for( let i=str.length;i>=0;i--){
+    // console.log(str.charAt(str.length-i)); 
+    reverse += str.charAt(i)
+      // revres i
+      // i = i+n  
+}
+console.log(reverse);
+
+
+//i=0,i<7
+//i=1,i<7
+//i=2,i<7
+

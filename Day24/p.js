9e4e1eef6f928c69b27d1381ce4ba2293b52730a
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-05 14:28:53 +0530

    done practice

---
+++ Day24/p.js
@@ -0,0 +1,14 @@
+function printPoem(){
+    console.log("Twinkle Twinkle little star");
+    console.log("How i wonder what you are");
+}
+printPoem();
+
+
+//roll dice
+
+function rollDice(){
+    let rand=Math.floor(Math.random()*6)+1;
+    console.log(rand);
+}
+rollDice();

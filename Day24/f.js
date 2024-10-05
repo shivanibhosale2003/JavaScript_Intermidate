7290591f8340b8aacc497853c5b24ed7ab737fd8
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-05 13:50:26 +0530

    learn function

---
+++ Day24/f.js
@@ -0,0 +1,21 @@
+function myName(){
+    console.log("Shivani Deshmukh");
+}
+//myName();
+
+function print1to10(){
+    for(i=1;i<=10;i++){
+        console.log(i);
+    }
+}
+//print1to10();
+
+function isAdult(){
+    let age=8;
+    if(age>=18){
+        console.log("Is adult");
+    }else{
+        console.log("Is not adult");
+    }
+}
+isAdult();

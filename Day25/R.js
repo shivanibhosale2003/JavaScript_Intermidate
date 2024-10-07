45edb45e1b872b87fbb93371020810bba538884b
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-07 12:55:22 +0530

    learn return

--- Day25/R.js
+++ Day25/R.js
@@ -3,3 +3,13 @@ function sum(x,y){
 }
 console.log(sum(1,2));
 
+
+function isAdult(age){
+    if(age>=18){
+        return "Adult";
+    }else {
+        return "Not adult";
+    }
+}
+console.log(isAdult(45));
+

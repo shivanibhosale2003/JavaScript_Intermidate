a003f1a1539e9de8f6422cbcf6c45592181520f4
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-08 11:59:51 +0530

    learn function experssion

---
+++ Day26/e.js
@@ -0,0 +1,12 @@
+let name=function(){
+    //console.log("Shivani");
+    return "Shivani";
+}
+
+
+let str=function(){
+    console.log("Hello");
+}
+str=function(){
+    console.log("How are you!"); 
+}

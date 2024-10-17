6ec633db39756a9a9d65537b74ef51157cbbb178
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-17 21:33:12 +0530

    learn filter

--- Day31/add.js
+++ Day31/add.js
@@ -3,3 +3,13 @@ let num=[1,2,3,4];
 let double=num.map(function (el){
     return el*el;
 });
+
+
+//Filter
+
+let nums=[1,2,3,4,5,6,8,10,12,14];
+
+let even=nums.filter((num)=>(num%2==0));
+
+let nums2=[22,23,25,26,27,29,33];
+let odd=nums2.filter((nums2)=>(nums2%2!=0));

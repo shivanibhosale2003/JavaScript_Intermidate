a7f6b69ceb855a1b3bc79c0b3672b9549c106d9c
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-22 18:21:21 +0530

    learn destructuring object

---
+++ Day34/o.js
@@ -0,0 +1,10 @@
+const student = {
+    name:"xyz",
+    class:9,
+    age:14,
+    subject:["English","hindi","math","marathi"],
+    username:"xyz@123",
+    password:"1903"
+};
+
+const { username:user , password:pass }=student;

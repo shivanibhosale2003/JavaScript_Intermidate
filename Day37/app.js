2c67e70f567d158acd972175054d6da9eb751145
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-28 11:48:16 +0530

    done practice

---
+++ Day37/app.js
@@ -0,0 +1,47 @@
+//let get=document.getElementById("mainImg");
+
+//console.log(get);
+
+
+//let gte2=document.getElementsByClassName("oldImg");
+
+//console.log(gte2[2]);
+
+
+//let g=document.getElementsByTagName("p");
+
+//console.log(g);
+
+
+//selector
+
+console.dir(document.querySelectorAll("h"));
+
+console.dir(document.querySelector("p"));
+
+console.dir(document.querySelector("#description"));
+
+console.dir(document.querySelector(".boxLink"));
+
+console.dir(document.querySelector("div li"));
+
+
+
+
+let img=document.querySelector('img');
+console.log(img);
+
+img.getAttribute('id');
+console.log(img);
+
+img.setAttribute('id','spidermanImg');
+
+
+
+let links=document.querySelectorAll(".box a");
+
+for(link of links){
+    link.style.color="red";
+}
+
+

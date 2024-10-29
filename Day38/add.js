0a0dbe950a23964a37c99621c3dc88fe451c2b65
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-29 18:32:45 +0530

    done practice

--- Day38/add.js
+++ Day38/add.js
@@ -1,6 +1,10 @@
 let btn=document.querySelector('button');
 console.dir(btn);
 
+btn.onclick=function(){
+console.log("button was clicked");
+};
+
 btn.onmouseenter=function () {
  console.log("buttob in an action");
 };

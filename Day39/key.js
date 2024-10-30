f52f149d982c4cb56e23a88616a809099d2efc52
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-30 18:03:36 +0530

    learn keyboard event

---
+++ Day39/key.js
@@ -0,0 +1,8 @@
+let inp=document.querySelector("input");
+
+inp.addEventListener("keyup",function(event){
+    console.log("key= ",event.key);
+    console.log("code= ",event.code);
+    
+console.log("key was pressed");
+});

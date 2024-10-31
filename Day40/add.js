fa8e448dabcd266d5f29d1a35101f23b888b2d4d
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-10-31 19:20:26 +0530

    done practice

--- Day40/add.js
+++ Day40/add.js
@@ -3,7 +3,12 @@ let form=document.querySelector("form");
 form.addEventListener("submit",function(event){
 event.preventDefault();
 
-let inp=document.querySelector("input");
-console.dir(inp);
-console.log(inp.value);
+let user=document.querySelector("#user");
+let pass=document.querySelector("#pass");
+
+console.log(user.value);
+console.log(pass.value);
+
+alert(`Hi ${user.value} ,your password is set to ${pass.value}`);
+
 });

283e96cf909ecff16472cfd60d1b366ac08372a9
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-11-08 12:19:00 +0530

    practice

--- Day46/add.js
+++ Day46/add.js
@@ -1,10 +1,10 @@
 function save(data){
-return new Promise((success,failure) => {
+return new Promise((resolve,reject) => {
     let internetSpeed=Math.floor(Math.random() * 10)+1;
     if(internetSpeed > 4){
-        success("Success: data was saved");
+        resolve("Success: data was saved");
     }else{
-        failure("weak connection");
+        reject("weak connection");
     }
 });
 }

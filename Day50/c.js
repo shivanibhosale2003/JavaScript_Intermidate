39e4295a1d66cc617f1ba17e93ba3fd0a095c037
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-11-13 14:16:42 +0530

    learn handling rejection

--- Day50/c.js
+++ Day50/c.js
@@ -3,6 +3,10 @@ h1=document.querySelector("h1");
 function changeColor(color,delay){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
+            let num=Math.floor(Math.random()*5)+1;
+            if(num >3){
+                reject("promise reject");
+            }
             h1.style.color=color;
             console.log(`color changed to ${color}!`);
              resolve("color cganged!");            
@@ -11,9 +15,21 @@ function changeColor(color,delay){
 }
 
 async function demo(){
-  await  changeColor("red",1000);
+ 
+ try{
+       await  changeColor("red",1000);
   await changeColor("pink",1000);
    await changeColor("purple",1000);
    await changeColor("orange",1000);
    await changeColor("blue",1000);
+ }catch(err){
+    console.log("error caught");
+    console.log(err);
+    
+ }
+
+
+   let a=5;
+   console.log(a);
+   console.log("new number=",a+5);
 }

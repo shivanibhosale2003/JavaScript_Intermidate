658b740c9472f46e726898d0117ef14e48ff092c
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2025-01-16 16:55:36 +0530

    practice

---
+++ Day64/shift.js
@@ -0,0 +1,5 @@
+let arr=[1,2,3,4];
+let shift=arr.shift();
+console.log(arr);
+console.log(shift);
+
---
+++ Day64/splice.js
@@ -0,0 +1,2 @@
+let colors=["red","pink","white","black","blue","purple","yellow","gold"];
+console.log(colors.splice(0,1,"grey","violet"));

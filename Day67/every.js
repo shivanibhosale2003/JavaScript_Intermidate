e13c24c07f09640ef0b20271a348d6d3e41ec1f7
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2025-01-23 17:06:09 +0530

    practice

--- Day67/every.js
+++ Day67/every.js
@@ -1,5 +1,6 @@
 let arr=[1,2,3,4,5];
 arr.every((num)=>{
-    console.log(num>0);
-    
-})
+    console.log(num>0);  
+});
+
+//every(callback):-check if all the conditions satisfy the condition
---
+++ Day67/some.js
@@ -0,0 +1,6 @@
+let arr=[1,2,3,4];
+arr.some((num)=>{
+     console.log(num>3);
+});
+
+//some(callback):-check if the one element satisfy the condition

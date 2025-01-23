a7b1e6ca91a6977891d103e1cf572bcdcc41cc96
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2025-01-23 16:37:33 +0530

    practice

---
+++ Day67/map.js
@@ -0,0 +1,7 @@
+let arr=[1,2,3,4];
+arr.map((num)=>{
+    console.log(num*2); 
+});
+
+//map(callback):-create a new array by applaing callback
+// to each element in array;                                                                       

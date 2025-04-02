db22f21e1e4a5f70e03cc97ab33a3eb973a6c83f
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2025-04-02 14:46:27 +0530

    practice

---
+++ Day69/add2.js
@@ -0,0 +1,9 @@
+let str=" bvhfirlllfll";
+console.log(str.lastIndexOf("l"));
+console.log(str.indexOf("v"));
+console.log(str.includes("i"));
+console.log(str.trim());
+console.log(str.startsWith(" bv"));
+console.log(str.endsWith("ll"));
+
+

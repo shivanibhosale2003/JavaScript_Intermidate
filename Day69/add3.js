7c3871bce0e4bd3d3238385f2a73b63841a34360
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2025-04-02 15:22:19 +0530

    practice

---
+++ Day69/add3.js
@@ -0,0 +1,11 @@
+let str="Hello shivani";
+let str1="unmesh";
+let str2="Bhosale";
+console.log(str.slice(1,5));
+console.log(str.replace("shivani","unmesh"));
+console.log(str.replaceAll("i","I"));
+console.log(str.split(","));
+console.log(str.concat(" ",str1, " " ,str2));
+console.log("1".padStart(5,"3"));
+console.log("1".padEnd(5,"3"));
+

5a9fef48aecbc9f0a0f37803d0980f17ea7e71d3
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2025-04-04 12:01:06 +0530

    practice

--- Day71/a.js
+++ Day71/a.js
@@ -3,6 +3,23 @@ arr.push(10,11);
 console.log(arr);
 
 let arr2=[1,2,34,45,6,7,77];
-let pop=arr.pop();
+let pop=arr2.pop();
 console.log(pop);
+
+
+let arr3=[1,2,3,4];
+arr3.splice(1,2,5,6);
+console.log(arr3);
+
+
+let arr4=[1,2,3,4,5];
+arr4.unshift(6,7);
+console.log(arr4);
+
+
+
+let arr5=[1,2,3,4,5];
+let shift=arr.shift();
+console.log(arr5);
+console.log(shift);
 
